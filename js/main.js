1a85ac1ec36ccffbe855928dc41518a8d27e6b30
AzAlex <azalexxx02@gmail.com>
2023-01-21 15:27:01 -0800

    move left

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 gsap.from('#redGirl', {
-  y: '1vh',
+  y: '2vh',
   delay: .5,
   duration: 3,
   repeat: -1,
   scrub: true,
   yoyo: true,
   ease: "power3.inOut",
+  
+})
+gsap.from('.container', {
+x: '-50vw',
+delay: 1,
+opacity: 0,
+duration: .5,
+
+
 })
 gsap.to('#Vector82_2', {
   stagger: 5,
@@ -23,4 +32,17 @@ gsap.to('#Vector_10_2', {
   opacity: 0,
   scrub: true,
 })
+
+$('.body').on('mousemove', (e) => {
+  const x = e.pageX / $(window).width();
+  const y = e.pageY / $(window).height();
+  $('.bg').css('transform',
+    'translatex(-' + x * 30 + 'px)'
+  );
+  $('#redGirl').css(
+    'transform',
+    'translate(+' + x * 30 + 'px, -' + y * 0 + 'px)'
+  );
+})
+
 
