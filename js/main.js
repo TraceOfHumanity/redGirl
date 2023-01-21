bba8acd9d7015d7a74974b854f3438efc6ebccdc
AzAlex <azalexxx02@gmail.com>
2023-01-21 00:12:58 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+gsap.from('#redGirl', {
+  y: '1vh',
+  delay: .5,
+  duration: 3,
+  repeat: -1,
+  scrub: true,
+  yoyo: true,
+  ease: "power3.inOut",
+})
+gsap.to('#Vector82_2', {
+  stagger: 5,
+  duration: 0.15,
+  repeat: -1,
+  repeatDelay: 4,
+  opacity: 0,
+  scrub: true,
+})
+gsap.to('#Vector_10_2', {
+  stagger: 5,
+  duration: 0.15,
+  repeat: -1,
+  repeatDelay: 4,
+  opacity: 0,
+  scrub: true,
+})
